18fca4c64044f7745f2cf26a5aa42dcd18f00de8
coolcode <coolcode@live.com>
2021-09-08 10:03:14 +0800

    init

---
+++ src/components/Menu/config.ts
@@ -0,0 +1,41 @@
+import { MenuEntry } from '@pancakeswap/uikit'
+import { ContextApi } from 'contexts/Localization/types'
+
+const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
+  {
+    label: t('Home'),
+    icon: 'HomeIcon',
+    href: '/',
+  },
+  
+  {
+    label: t('Farms'),
+    icon: 'FarmIcon',
+    href: '/farms',
+  },
+  {
+    label: t('Pools'),
+    icon: 'PoolIcon',
+    href: '/pools',
+  }
+]
+
+/* {
+    label: t('Trade'),
+    icon: 'TradeIcon',
+    items: [
+      {
+        label: t('Exchange'),
+        href: '/swap',
+      },
+      {
+        label: t('Liquidity'),
+        href: '/liquidity',
+      },
+      {
+        label: t('LP Migration'),
+        href: 'https://v1exchange.pancakeswap.finance/#/migrate',
+      },
+    ],
+  }, */
+export default config
